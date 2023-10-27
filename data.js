e3e39c74e8e639269eef4ecad2f0ae45b3a13bbc
avinasxhhh <avinashmundhe1050@gmail.com>
2023-10-27 19:34:16 +0530

    fisrt1 commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+    name: "falcon and the winter soldier",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestias, reprehenderit tempora sit quod soluta!",
+    image: "images/slider 2.png"
+    },
+    {
+    name: "loki",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestias, reprehenderit tempora sit quod soluta!",
+    image: "images/slider 1.png"
+    },
+    {
+    name: "wanda vision",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestias, reprehenderit tempora sit quod soluta!",
+    image: "images/slider 3.png"
+    },
+    {
+    name: "raya and the last dragon",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestias, reprehenderit tempora sit quod soluta!",
+    image: "images slider 4.png"
+    },
+    {
+    name: "Luca",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestias, reprehenderit tempora sit quod soluta!",
+    image: "images/slider 5.png"
+    }
+]
